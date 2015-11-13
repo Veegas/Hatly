69e46d778222afb34f98b58d133cfc46cc4d3ccd
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-11-13 19:02:03 +0200

    User model

---
+++ db/migrate/20151112180339_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.string :email
+      t.string :password_digest
+      t.string :city
+      t.string  :country
+      t.string :confirm_password
+
+      t.timestamps null: false
+    end
+  end
+end

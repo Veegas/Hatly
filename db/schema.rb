a6fc4b11b3dbef40989822f01fc349fe8da74970
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-11-13 20:07:13 +0200

    Added posts and items model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112180339) do
+ActiveRecord::Schema.define(version: 20151113174946) do
+
+  create_table "items", force: :cascade do |t|
+    t.float    "price"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "type",       null: false
+    t.text     "text",       null: false
+    t.string   "location",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"

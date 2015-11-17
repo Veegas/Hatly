e4ff0f59c1c08e37c95af208b534a75f56546c40
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-11-17 21:12:20 +0200

    Added incode comments in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
+#  Virtual Attributes
   has_secure_password
 
+#  Relations
   has_many :comments
   has_many :friendships, :foreign_key => "user", 
   :class_name => "Friendship"
-  has_many :friends, :through => :friendships
-  
+  has_many :friends, :through => :friendships  
   has_many :posts
   has_many :wall_posts
 
+#  Validations
   validates :email, email_format: { message: "Isn't in Email format" }
-
   validates :password, :length => {:within => 6..40}
-
 end
